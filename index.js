094a8f4cb8c5bcb2281bc67065c753b9968188a9
Kumar <kumarnaidukarri2002@gmail.com>
2025-09-24 00:03:00 +0530

    1 - Basic App with navbar, card components, DesignDocument

---
+++ index.js
@@ -0,0 +1,84 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/*
+Components Design:
+AppLayout
+1) Header
+    - Logo
+    - Navitems
+
+2) Body
+    - Search bar
+    - RestaurantsContainer
+        - RestaurantCard
+            -Img
+            -Name of Res, Rating, Cuisine, delivery time
+
+3) Footer 
+    - copyright
+    - links
+    - address
+    - contact
+*/
+
+const Header = function () {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://png.pngtree.com/png-vector/20220708/ourmid/pngtree-fast-food-logo-png-image_5763171.png"
+          alt="food logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = function () {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/3/17/6a4369e1-0c9c-49f9-8132-5be682a7831f_1046503.jpg"
+        alt="res img"
+      />
+      <h3> Guma Gumalu Foods </h3>
+      <h4> Biryani, North Indian, Asian </h4>
+      <h4> 4.5 stars</h4>
+      <h4> 38 minutes</h4>
+    </div>
+  );
+};
+
+const Body = function () {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = function () {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const rootEl = ReactDOM.createRoot(document.getElementById("root"));
+rootEl.render(<AppLayout />);
