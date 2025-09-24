3b94e1eddf597dee59268a78a398c87d3c103e44
Kumar <kumarnaidukarri2002@gmail.com>
2025-09-24 12:22:47 +0530

    1.1 - Refined config-files DesignDocument

--- index.js
+++ index.js
@@ -66,6 +66,15 @@ const Body = function () {
       <div className="search">Search</div>
       <div className="res-container">
         <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
       </div>
     </div>
   );
