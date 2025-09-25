7a7e2aff6e784b1c813e07142895051ab22f6442
Kumar <kumarnaidukarri2002@gmail.com>
2025-09-25 23:59:24 +0530

    2.2 - Implemented HOOKS to filter Cards and re-render them

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,36 @@
+import { useState } from "react";
+
 import RestaurantCard from "./RestaurantCard.js";
 import resList from "../utils/mockData.js";
 
 const Body = function () {
+  // Creates local State variable
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      {/* <div className="search">Search</div> */}
+
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // Filter Logic here
+            console.log("Button clicked!");
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+
       <div className="res-container">
         {
           // Dynamically Rendering ResCards
-          resList.map((restaurant) => (
+          listOfRestaurants.map((restaurant) => (
             <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           ))
         }
