e2db070a83bbcff8453fe662f5b208b2fedf9947
Kumar <kumarnaidukarri2002@gmail.com>
2025-10-06 00:04:32 +0530

    4.3 - Page Navigation using 'Link' component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
-import { LOGO_URL } from "../utils/constants.js";
-
 import { useState } from "react";
+import { Link } from "react-router"; // Link component
+
+import { LOGO_URL } from "../utils/constants.js";
 
 const Header = function () {
   // useState() creates a local state variable
@@ -13,9 +14,15 @@ const Header = function () {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/"> Home </Link>
+          </li>
+          <li>
+            <Link to="/about">About Us </Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
           <button
             className="login-btn"
