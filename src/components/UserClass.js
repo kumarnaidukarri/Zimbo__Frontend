49156fa2774baabf01314dfe8ab0774190cc93e1
Kumar <kumarnaidukarri2002@gmail.com>
2025-10-27 17:22:21 +0530

    5.3 - Lifecycle Methods of Class components

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,20 +1,69 @@
 import React from "react";
 
-// 'User' class component similar to 'User' functional component
+// Class Components, its Lifecycle methods are Outdated. But still IMP for fundamentals.
+/* 
+Phases -
+ 1. Mounting Phase:
+    i)  constructor()
+    ii) render()
+    iii) componentDidMount()
 
+ 2. Updating Phase:
+    i)   shouldComponentUpdate()
+    ii)  render()        
+    iii) componentDidUpdate()
+
+ 3. Unmounting Phase:
+    componentWillUnmount()   
+*/
+
+// 'User' class component
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      userInfo: {
+        name: "Dummy",
+        location: "Default",
+      },
+    };
     console.log(this.props.name + " constructor");
   }
-
   render() {
+    const { name, location, avatar_url } = this.state.userInfo;
     console.log(this.props.name + " render");
-    return <h1> hi {this.props.name} </h1>;
+    return (
+      <div className="user-card">
+        <img src={avatar_url} />
+        <h2> Name: {name} </h2>
+        <h3> Location: {location} </h3>
+        <h4> Contact: </h4>
+      </div>
+    );
+  }
+  async componentDidMount() {
+    // runs in Mounting Phase
+    console.log(this.props.name + " componentDidMount");
+
+    // API call to public github
+    const responseObj = await fetch(
+      "https://api.github.com/users/mosh-hamedani"
+    );
+    const jsonData = await responseObj.json();
+
+    this.setState({ userInfo: jsonData }); // state update
   }
 
-  componentDidMount() {
-    console.log(this.props.name + " componentDidMount");
+  shouldComponentUpdate(prevProps, prevState) {
+    // runs at begining of Updating Phase
+    console.log(this.props.name + " ShouldUpdate");
+    console.log("Attention: ", prevProps, prevState);
+    return true; // true(re-render) false(no re-render)
+  }
+  componentDidUpdate() {
+    // runs at ending of Updating Phase
+    console.log(this.props.name + " componentDidUpdate");
   }
 }
 
