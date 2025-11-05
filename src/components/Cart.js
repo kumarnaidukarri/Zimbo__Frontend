bc4776628ed78f8393416a85d4944a6dd4e9c21b
Kumar <kumarnaidukarri2002@gmail.com>
2025-11-05 22:45:19 +0530

    10.5 - Implemented Clear Cart functionality, fixed small key error in list rendering

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,36 @@
 // Cart page component
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { clearCart } from "../utils/Store/cartSlice.js";
 
 const Cart = () => {
   // useSelector() hook used to get data 'items' from the Cart Slice of Redux Store.
   const cartItems = useSelector((store) => store.cart.items);
 
+  const dispatch = useDispatch(); // useDispatch() hook
+
+  const handleClearCart = () => {
+    dispatch(clearCart()); // dispatch(action())
+  };
+
   return (
     <div className="text-center m-4 p-4">
       <h1 className="text-2xl font-bold">Cart</h1>
+
+      {cartItems.length === 0 ? (
+        <h1 className="mt-4">Cart is empty. Add Items to the Cart!</h1>
+      ) : (
+        <button
+          className="px-4 py-2 m-4 bg-black text-white rounded-lg"
+          onClick={handleClearCart}
+        >
+          Clear Cart
+        </button>
+      )}
       <ul className="cart-list  px-3 py-1 w-6/12 m-auto">
-        {cartItems.map((item) => (
+        {cartItems.map((item, index, arr) => (
           <li
-            key={item.itemId}
+            key={index}
             className="my-3 first:mt-0 last:mb-0 text-lg flex justify-between"
           >
             <span>{`${item.itemName} - Rs.${item.price}/-`}</span>
